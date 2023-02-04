5e698108dfb634cc186575eba1fe1c007398e51e
Илья Пирогов <ilya@MacBook-Air-Ila.local>
2023-02-05 00:02:36 +0300

    adds table

--- src/components/Home/Tab.jsx
+++ src/components/Home/Tab.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Tab = (props) => {
   return (
@@ -11,7 +11,7 @@ const Tab = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tab
+export default Tab;
--- src/components/Home/Tabs.jsx
+++ src/components/Home/Tabs.jsx
@@ -1,88 +1,195 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
 
-const tabs = [
-  {
-    id: 1,
-    title: 'Свойства',
-    description:
-      '<ul><li>НОРМАЛИЗУЕТ МИНЕРАЛЬНЫЙ СОСТАВ И ПРОЧНОСТЬ КОСТНОЙ ТКАНИ.</li><li>НОРМАЛИЗУЕТ ЗДОРОВЬЕ МЫШЦ, ХРЯЩЕВЫХ И СОЕДИНИТЕЛЬНЫХ ТКАНЕЙ.</li><li>СНИЖАЕТ ДИСКОМФОРТ В СУСТАВАХ ПРИ ФИЗИЧЕСКИХ НАГРУЗКАХ.</li><li>СПОСОБСТВУЕТ СНИЖЕНИЮ РИСКА ПЕРЕЛОМОВ И ДРУГИХ ТРАВМ.</li><li>СПОСОБСТВУЕТ МЕТАБОЛИЗМУ БЕЛКА.</li></></ul>',
-  },
-  {
-    id: 2,
-    title: 'Состав',
-    description:
-      'ЦИНК помогает поддерЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический ЦИНК помогает поддерживать нормальный химическийЦИНК помогает поддерживать нормальный химический живать нормальный химический состав костей, способствует поддержанию нормального уровня тестостерона в крови',
-  },
-  {
-    id: 3,
-    title: 'Документы',
-    description:
-    'ВИТАМИН К2 способствует поддержанию нормального состава костей, способствует лечению остеопороза, помогает поддерживать прочность костей в области шейки бедра у женщин в постменопаузе',
-  },
-  {
-    id: 4,
-    title: 'Рекомендации',
-    description:
-      'ВИТАМИН D3 способствует поддержанию нормального состава костей, способствует нормальному содержанию кальция в сыворотке крови.',
-  },
-  {
-    id: 5,
-    title: 'Форма выпуска',
-    description:
-      'ВИТАМИН B6 способствует нормальному метаболизму белка',
-  },
-]
 const Tabs = () => {
-  const [actualStep, setActualStep] = useState({
-    activeObject: tabs[0],
-    objects: tabs,
-  })
-
-  const toggleActive = (index) => {
-    setActualStep({
-      ...actualStep,
-      activeObject: actualStep.objects[index],
-    })
-  }
-
-  function toggleActiveTexts(index) {
-    if (actualStep.objects[index] === actualStep.activeObject) {
-      return 'Tabs__desc-item active'
-    } else {
-      return 'Tabs__desc-item inactive'
-    }
-  }
-
-
-  function toggleActiveStyles(index) {
-    if (actualStep.objects[index] === actualStep.activeObject) {
-      return 'Tabs__item active'
-    } else {
-      return 'Tabs__item inactive'
-    }
-  }
   return (
     <section className="Tabs">
-      <div className="Container__inner">
+      <div class="Container__inner">
         <div className="Tabs__items">
-            {actualStep.objects.map((item, index) => (
-              <div
-                key={index}
-                className={toggleActiveStyles(index)}
-                onClick={() => {
-                  toggleActive(index)
-                }}
-              >
-                 <p className='Tabs__item-title'>{item.title}</p>
-                 <p className="Tabs__item-desc" dangerouslySetInnerHTML={{ __html: item.description }} ></p>
+          <div className="Tabs__item">
+            <a href="#first" id="first" className="Tabs__item-title">
+              Свойства
+            </a>
+            <a href="#first" class="Tabs__item-info">
+              Свойства
+            </a>
+            <div class="Tabs__item-desc">
+              <div className="Tabs__item-desc-inner">
+                <ul>
+                  <li>
+                    Нормализует минеральный состав и прочность костной ткани;
+                  </li>
+                  <li>
+                    Улучшает здоровье мышц, хрящевых и соединительных тканей;
+                  </li>
+                  <li>
+                    Борется с дискомфортом в суставах при физических нагрузках;
+                  </li>
+                  <li>Снижает риск переломов и других травм;</li>
+                  <li>Улучшает метаболизм белка.</li>
+                </ul>
               </div>
-            ))}
-
+            </div>
+          </div>
+          <div className="Tabs__item">
+            <a href="#second" id="second" class="Tabs__item-title">
+              Состав
+            </a>
+            <a href="#second" class="Tabs__item-info">
+              Состав
+            </a>
+            <div class="Tabs__item-desc">
+              <div className="Tabs__item-desc-inner">
+                <p className="Tabs__item-composition">
+                  Ингредиенты, которые содержатся в одной капсуле:
+                </p>
+                <div class="Composition">
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      Морские минералы из цельных известковых морских водорослей
+                      (Lithothamnium sp.).
+                    </div>
+                    <div class="Composition__value">834 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе кальций
+                    </div>
+                    <div class="Composition__value">250 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">Сульфат цинка</div>
+                    <div class="Composition__value">27,47 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе цинк
+                    </div>
+                    <div class="Composition__value">10 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      Бамбуковый экссудат
+                    </div>
+                    <div class="Composition__value">20 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе кремний
+                    </div>
+                    <div class="Composition__value">15 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      Магниевые соли жирных кислот
+                    </div>
+                    <div class="Composition__value">10 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">Менахинон 7</div>
+                    <div class="Composition__value">7 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе витамин К2
+                    </div>
+                    <div class="Composition__value">11,25 мкг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">Цианокобаламин</div>
+                    <div class="Composition__value">3,5 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе витамин B12
+                    </div>
+                    <div class="Composition__value">2,8 мкг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      Пиридоксина гидрохлорид
+                    </div>
+                    <div class="Composition__value">2,98 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе витамин B6
+                    </div>
+                    <div class="Composition__value">2 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">Холекальциферол</div>
+                    <div class="Composition__value">2,5 мг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе витамин D3
+                    </div>
+                    <div class="Composition__value">5 мкг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      Фолиевая кислота
+                    </div>
+                    <div class="Composition__value">360 мкг</div>
+                  </div>
+                  <div class="Composition__row">
+                    <div class="Composition__text">
+                      в том числе витамин B9
+                    </div>
+                    <div class="Composition__value">250 мкг</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="Tabs__item">
+            <a href="#third" id="third" class="Tabs__item-title">
+              Документы
+            </a>
+            <a href="#third" class="Tabs__item-info">
+              Документы
+            </a>
+            <div class="Tabs__item-desc">
+              <div className="Tabs__item-desc-inner">
+                <a
+                  href="https://projectvint.ru/storage/store/catalog/161/os-ru.pdf?v=1624041185"
+                  target="_blank"
+                >
+                  Сертификат
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="Tabs__item">
+            <a href="#fourth" id="fourth" class="Tabs__item-title">
+              Рекомендации
+            </a>
+            <a href="#fourth" class="Tabs__item-info">
+              Рекомендации
+            </a>
+            <div class="Tabs__item-desc">
+              <div className="Tabs__item-desc-inner">
+              Рекомендуется в качестве биологически активной добавки к пище — источника кальция, цинка и витаминов групп D, B и K.                            
+              </div>
+            </div>
+          </div>
+          <div className="Tabs__item">
+            <a href="#fifth" id="fifth" class="Tabs__item-title">
+              Форма выпуска
+            </a>
+            <a href="#fifth" class="Tabs__item-info">
+              Форма выпуска
+            </a>
+            <div class="Tabs__item-desc">
+              <div className="Tabs__item-desc-inner">
+                30 капсул
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
